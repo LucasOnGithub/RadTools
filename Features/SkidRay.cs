50354b2b1d1c818c597ffcd63b9b224bf1aa7748
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2026-06-06 05:14:02 -0400

    Skid ray

--- Features/SkidRay.cs
+++ Features/SkidRay.cs
@@ -3,6 +3,7 @@ using BepInEx.Unity.IL2CPP.Utils.Collections;
 using Photon.Pun;
 using Photon.Realtime;
 using RadTools.Core;
+using RadTools.Extensions;
 using RadTools.Libraries.RadiumWrapper;
 using System;
 using System.Collections;
@@ -11,6 +12,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using static UnityEngine.GridBrushBase;
 
 namespace RadTools.Features
 {
@@ -24,11 +26,84 @@ namespace RadTools.Features
             get => toggle != null && toggle.BOHIGPFGJBA;
         }
 
-        internal void Awake() =>
+        internal void Awake()
+        {
             Instance = this;
+            Libraries.RadiumWrapper.Patches.Tools.ToolInstantiated += (tool) =>
+            {
+                if (!Active)
+                    return;
+
+                var photonView = tool.photonView;
+                if (photonView == null)
+                    return;
+
+                var creator = PhotonNetwork.NetworkingClient.CurrentRoom.GetPlayer(photonView.CreatorActorNr);
+                if (creator == null)
+                    creator = PhotonNetwork.NetworkingClient.CurrentRoom.GetPlayer(photonView.OwnerActorNr);
+                
+                if ((new[]
+                {
+                    "[feedbacktool]",
+                    "[makerpen]",
+                    "[sharecamera]",
+                    "bowstring",
+                    "pulltab",
+                    "makerpenpalettemoveabletoolmenu",
+                    "[giftbox]",
+                    "creationshapecontainer"
+                }).Any(tool.gameObject.name.ToLower().Contains))
+                    return;
+
+                Notifications.Notify($"New prefab {tool.gameObject.name} created by {creator?.NickName ?? "Unknown"}", tool.gameObject.name.ToLower().Contains("consumable") ? Color.white : Color.red);
+            };
+        }
+
+        private float notificationTime;
+        private bool previousEnabled;
 
         public void Update()
         {
+            if (Active && !previousEnabled)
+            {
+                Notifications.Notify("Notifications for prefabs will appear. Don't leave enabled for long, causes performance drops", Color.yellow, 5f);
+            }
+            previousEnabled = Active;
+
+            if (Active && Time.time > notificationTime)
+            {
+                var tools = GameObj.GetAllType<Tool>(1f);
+                foreach (var tool in tools)
+                {
+                    if (tool == null) continue;
+                    if (new[] { PlayerRoot.LeftHand, PlayerRoot.RightHand }.Any(x => x.OIDGJKOGAIJ == tool || Vector3.Distance(x.transform.position, tool.gameObject.transform.position) > 1f))
+                        continue;
+
+                    if ((new[]
+                    {
+                        //"[feedbacktool]",
+                        //"[makerpen]",
+                        //"[sharecamera]",
+                        "bowstring",
+                        "pulltab",
+                        "makerpenpalettemoveabletoolmenu",
+                        "[giftbox]",
+                        "creationshapecontainer"
+                    }).Any(tool.gameObject.name.ToLower().Contains))
+                            continue;
+
+                    var photonView = tool.photonView;
+                    if (photonView == null)
+                        continue;
+                    var creator = PhotonNetwork.NetworkingClient.CurrentRoom.GetPlayer(photonView.CreatorActorNr);
+                    if (creator == null)
+                        creator = PhotonNetwork.NetworkingClient.CurrentRoom.GetPlayer(photonView.OwnerActorNr);
+
+                    notificationTime = Time.time + 1f;
+                    Notifications.Notify($"{tool.gameObject.name} created by {creator?.NickName ?? "Unknown"}", tool.gameObject.name.ToLower().Contains("consumable") ? Color.white : Color.red);
+                }
+            }
+
             var screen = GameObject.Find("PlayerRoot/[Player](Clone)_local/TrackingSpace/WatchMenu/VisualRoot/[PlayerWatchMenu](Clone)/Canvas/Screens/[SettingsScreen](Clone)/Body/[AdvancedSubscreen]/Options");
             if (screen == null)
                 return;
@@ -43,6 +118,7 @@ namespace RadTools.Features
 
             skidRay = Instantiate(showNames);
             skidRay.transform.SetParent(showNames.transform.parent, false);
+            skidRay.name = "SkidRay";
 
             skidRay.transform.Find("Text").GetComponent<TMPro.TextMeshProUGUI>().text = "Skid Ray";
             toggle = skidRay.transform.Find("Container/ShowNames_Toggle").GetComponent<ToggleButton3D>();
