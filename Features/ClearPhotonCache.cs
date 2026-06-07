072d2157030d08bb99b9b2f4ba71614a4ab6f4f1
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2026-06-07 15:07:15 -0400

    clear photon cache

---
+++ Features/ClearPhotonCache.cs
@@ -0,0 +1,39 @@
+﻿using AGUI.StackedUI;
+using BepInEx.Unity.IL2CPP.Utils.Collections;
+using Photon.Pun;
+using Photon.Realtime;
+using RadTools.Core;
+using RadTools.Extensions;
+using RadTools.Libraries.RadiumWrapper;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using static UnityEngine.GridBrushBase;
+
+namespace RadTools.Features
+{
+    [Instance.AddOnAwake]
+    internal class ClearPhotonCache : MonoWrap
+    {
+        internal static ClearPhotonCache Instance;
+
+        internal void Awake()
+        {
+            Instance = this;
+            if (PlayerPrefs.HasKey("VoiceCloudBestRegion"))
+            {
+                PlayerPrefs.DeleteKey("VoiceCloudBestRegion");
+                PlayerPrefs.Save();
+            }
+            if (PlayerPrefs.HasKey("PUNCloudBestRegion"))
+            {
+                PlayerPrefs.DeleteKey("PUNCloudBestRegion");
+                PlayerPrefs.Save();
+            }
+        }
+    }
+}
